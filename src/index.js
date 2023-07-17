1f459a2be0e5f5397abc1e0a33ab134b2b225b7d
scobett <kevin@Scobett>
2023-07-17 23:14:20 +0300

    Complete Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+const form = document.getElementById("myForm");
+
+form.addEventListener("submit", function(event) {
+ 
+  event.preventDefault();
+
+  const input = form.querySelector("input[type='text']");
+  const task = input.value;
+
+  console.log("New Task:", task);
+
+  input.value = "";
+});
+
